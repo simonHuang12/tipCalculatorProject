36b510d515cc663ae831adbf9dfba0bb687dc2ad
simonHuang12 <simonh8@nycstudents.net>
2022-09-28 22:21:47 -0400

    2

--- src/main.java
+++ src/main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class main {
+import java.util.Scanner;
+public class main
+{
+    public static void Main(String[] args)
+    {
+        Scanner input = new Scanner(System.in); //creates a new scanner
+        System.out.print("What is the total bill?"); //bill info
+        double totalBill = input.nextDouble();
+        System.out.print("What is the tip percentage as a whole number (without % symbol)?"); //percent info
+        double tipPercentage = input.nextDouble();
+        System.out.print("How many people are paying?"); //number of people paying
+        int numberOfPeople = input.nextInt();
+
+
+    }
 }
