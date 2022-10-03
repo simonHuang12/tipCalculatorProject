5305764303e9ca65ba4763190da8a1f2214a008f
simonHuang12 <simonh8@nycstudents.net>
2022-10-02 21:13:02 -0400

    3

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class main
 {
-    public static void Main(String[] args)
+    public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in); //creates a new scanner
         System.out.print("What is the total bill?"); //bill info
@@ -11,15 +11,16 @@ public class main
         System.out.print("How many people are paying?"); //number of people paying
         int numberOfPeople = input.nextInt();
 
-        double totalTip = totalBill * (tipPercent / 100); // calculates the tip
-        totalBill += totalTip; // adds the tip to the total bill
-        double totalPerPerson = totalBill / numberOfPeople; // calculates how much each person pays
+        double totalTip = totalBill*(tipPercent/100); // calculates the tip
+        totalBill+=totalTip; // adds the tip to the total bill
+        double totalPerPerson = totalBill/numberOfPeople; // calculates how much each person pays
+        double tipPerPerson = totalTip/numberOfPeople;
 
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); //receipt
         System.out.printf("Total Tip Amount: $%.2f\n", totalTip);
-        System.out.printf("Total Bill With Tip: $%.2f\n", totalBill);
-        System.out.printf("Tip Per Person: $%.2f\n", totalPerPerson);
-        System.out.printf("Total Cost Per Person: $%.2f\n", totalPerPerson);
-        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.printf("Total Bill Including Tip: $%.2f\n", totalBill);
+        System.out.printf("Tip Per Person: $%.2f\n", tipPerPerson);
+        System.out.printf("Total Per Person: $%.2f\n", totalPerPerson);
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
     }
 }
